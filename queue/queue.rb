dd6f9eb20e70af529a895190979ffec94c5eab0e
Antonio Perez <antonio.perez.maldonado@icalialabs.com>
2021-07-27 12:59:04 -0500

    Implement simple queue data structure

---
+++ queue/queue.rb
@@ -0,0 +1,43 @@
+class Queue
+  attr_reader :head
+
+  def initialize
+    @head = nil
+    @queue = Array.new
+  end
+
+  def enqueue(item)
+    @queue << push item
+    @head = @queue[@queue.size-1]
+  end
+
+  def dequeue
+    return nil if @queue.size.zero?
+
+    @queue.pop
+    @head = @queue[@queue.size-1]
+  end
+
+  def head_item
+    @head
+  end
+
+  def clear_queue
+    @queue.clear
+  end
+
+  private
+
+  def display_empty_queue
+    puts '-'*10
+    puts '|        |'
+    puts '-'*10
+  end
+
+  def display_queue_body
+    puts '-'*10
+    @queue.each { |element| puts "#{element.to_s.center( 8, ' ')}"}
+    puts '-'*10
+  end
+
+end
